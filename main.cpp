2fa58980578a3e6535ed8f90696ca552b5902e66
Artemiy <artemiy.borodulin@gmail.com>
2023-11-13 14:19:15 +0500

    Initial comit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
-#include <iostream>
+#include <Windows.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    if (uMsg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    else {
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    const char CLASS_NAME[] = "MyWindowClass";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+            0,                      // Optional window styles
+            CLASS_NAME,             // Window class
+            "WinAPI App",           // Window text
+            WS_OVERLAPPEDWINDOW,    // Window style
+            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,   // Size and position
+            nullptr,                   // Parent window
+            nullptr,                   // Menu
+            hInstance,              // Instance handle
+            nullptr                    // Additional application data
+    );
+
+    if (hwnd == nullptr) {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
     return 0;
 }
